26f09a3a3c59739b86adc117d08a9806fe524c71
aosoft <tan-y@aosoft.jp>
2019-11-10 13:53:43 +0900

    Add RunKernelParallelOpenCV

--- CppRayMarching/kernel.cpp
+++ CppRayMarching/kernel.cpp
@@ -4,7 +4,7 @@
 #include <thread>
 #include <future>
 
-void RunKernel(std::int32_t width, std::int32_t height, void* p, std::int32_t stride, float time, FnKernel fnKernel)
+void RunKernelSingle(std::int32_t width, std::int32_t height, void* p, std::int32_t stride, float time, FnKernel fnKernel)
 {
 	auto resolution = glm::vec2(static_cast<float>(width), static_cast<float>(height));
 
@@ -54,4 +54,21 @@ void RunKernelParallel(std::int32_t width, std::int32_t height, void* p, std::in
 	{
 		futures[i].wait();
 	}
+}
+
+void RunKernelParallelOpenCV(std::int32_t width, std::int32_t height, void* p, std::int32_t stride, float time, FnKernel fnKernel)
+{
+	cv::parallel_for_(cv::Range(0, height), [width, height, p, stride, time, fnKernel](const cv::Range& range)
+		{
+			auto resolution = glm::vec2(static_cast<float>(width), static_cast<float>(height));
+			for (std::int32_t y = range.start; y < range.end; y++)
+			{
+				auto p2 = reinterpret_cast<glm::vec4*>(static_cast<std::uint8_t*>(p) + y * stride);
+				for (std::int32_t x = 0; x < width; x++)
+				{
+					fnKernel(*p2, glm::vec2(static_cast<float>(x) / resolution.x, static_cast<float>(y) / resolution.y), time, resolution);
+					p2++;
+				}
+			}
+		});
 }
--- CppRayMarching/kernel.h
+++ CppRayMarching/kernel.h
@@ -5,5 +5,6 @@
 
 using FnKernel = void (*)(glm::vec4& gl_FragColor, const glm::vec2 gl_FragCoord, float time, const glm::vec2 resolution);
 
-void RunKernel(std::int32_t width, std::int32_t height, void* p, std::int32_t stride, float time, FnKernel fnKernel);
+void RunKernelSingle(std::int32_t width, std::int32_t height, void* p, std::int32_t stride, float time, FnKernel fnKernel);
 void RunKernelParallel(std::int32_t width, std::int32_t height, void* p, std::int32_t stride, float time, FnKernel fnKernel);
+void RunKernelParallelOpenCV(std::int32_t width, std::int32_t height, void* p, std::int32_t stride, float time, FnKernel fnKernel);
